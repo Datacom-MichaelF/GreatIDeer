7ccbec84a73b627177bb75ef787a8fc0289c7de7
Kawindu Lokuge <Kawinduelk@gmail.com>
2019-05-19 10:35:36 +1200

    lastest changes 10:35

--- GoodIDeer/Pages/Camera1.cshtml.cs
+++ GoodIDeer/Pages/Camera1.cshtml.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GoodIDeer.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using GoodIDeer.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace GoodIDeer.Pages
 {
     public class Camera1Model : PageModel
     {
-        public void OnGet()
+        public async Task OnGet()
         {
             string imageFilePath = @"C:\Work\Other\GoodDeer\GoodIDeer\images\deer01.jpg";
 
             VisionApiService MakeAnalysis = new VisionApiService();
-            var response = MakeAnalysis.MakeAnalysisRequest(imageFilePath);
+            /*var respose =*/
+            var response = await MakeAnalysis.MakeAnalysisRequest(imageFilePath);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var thing = JsonConvert.DeserializeObject<Model.RootObject>(content);
         }
     }
 }
--- GoodIDeer/Pages/Index.cshtml.cs
+++ GoodIDeer/Pages/Index.cshtml.cs
@@ -1,24 +1,15 @@
 ﻿using GoodIDeer.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace GoodIDeer.Pages
 {
     public class IndexModel : PageModel
     {
-
-        public string Response { get; set; }
-
-        public async Task OnGet()
+        public void OnGet()
         {
-            string imageFilePath = @"C:\Work\Other\GoodDeer\GoodIDeer\images\deer01.jpg";
-
-            VisionApiService MakeAnalysis = new VisionApiService();
-            /*var respose =*/
-            var response = await MakeAnalysis.MakeAnalysisRequest(imageFilePath);
-
-            var content = await response.Content.ReadAsStringAsync();
-            var thing = JsonConvert.DeserializeObject<Model.RootObject>(content);
-            Response = thing;
+            
         }
 
         public void processImage()
